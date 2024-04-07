45cf80ac065f3e6e73f715d0e6c4f8f557df9f3d
Samarth320 <140296067+Samarth320@users.noreply.github.com>
2024-04-07 12:13:47 +0530

    Add files via upload

---
+++ print_kth_RECURSION_APPROACH.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+using namespace std;
+
+class Node
+{
+	public:
+         int data;
+         Node* next;
+   
+   Node(int data)
+   {
+   	this->data = data;
+   	this->next = NULL;
+   }
+   	
+};
+
+void print (Node* head)
+{
+	Node* temp = head;
+	while(temp != NULL)
+	{
+		cout<<temp->data<<" ";
+		temp = temp->next;
+	}
+	cout<<endl;
+}
+
+void print_kth(Node* head , int &pos)
+{
+	// base case
+	if(head == NULL)
+	return;
+	
+	print_kth(head->next , pos);
+	
+	if(pos == 0)
+	{
+		cout<<"the value is "<<head->data<<endl;
+	}
+	
+	pos--;
+}
+
+int main()
+{ 
+   Node* x = new Node(2);
+   Node* y = new Node(3);
+   Node* z = new Node(4);
+   Node* a = new Node(5);
+   Node* b = new Node(6);
+    
+    
+    x->next = y;
+    y->next = z;
+    z->next = a;
+    a->next = b;
+    b->next = NULL; 
+    
+    Node* head = x;
+    
+    int pos = 2;
+	print(head); 
+	print_kth(head , pos);
+    
+    return 0;
+}
