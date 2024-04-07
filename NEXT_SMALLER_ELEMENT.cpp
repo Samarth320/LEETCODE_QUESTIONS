36cac2f4613f9b239265eb467f8247b15cf7c527
Samarth320 <140296067+Samarth320@users.noreply.github.com>
2024-04-07 12:17:35 +0530

    Add files via upload

---
+++ NEXT_SMALLER_ELEMENT.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+using namespace std;
+
+vector<int> Next_smaller(vector<int> &input)
+{
+	stack<int> s;
+	s.push(-1);
+	
+	vector<int> ans( input.size() );
+	
+	for(int i=input.size()-1; i>=0; i--)
+	{
+		int curr = input[i];
+		
+		// curr tum apne liye next smaller element stack mai se dhoond ke lau
+		
+		while(s.top() >= curr)
+		{
+			s.pop();
+		}
+		
+		// now the top element is smaller than curr, so we store it as a ans
+		
+		ans[i] = s.top();
+		
+		// now we push the curr in stack
+		s.push(curr);
+	}
+	return ans;
+}
+
+int main()
+{
+	vector<int> input;
+	input.push_back(2);
+	input.push_back(1);
+	input.push_back(4);
+	input.push_back(3);
+	
+	vector<int> ans = Next_smaller(input);
+	
+	cout<<"printing ans"<<endl;
+	for(int i=0; i<ans.size(); i++)
+	{
+		cout<<ans[i]<<" ";
+	}
+	cout<<endl;
+	
+	
+	return 0;
+}
